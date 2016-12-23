67b0ea2bba3b2ad6a54e54ef156176865b232bdd
David Riesz <dbriesz@users.noreply.github.com>
2016-12-23 13:55:58 -0800

    Replaced new.hbs with edit.hbs.  Added form elements for username and password prompts.

--- src/main/java/com/teamtreehouse/blog/Main.java
+++ src/main/java/com/teamtreehouse/blog/Main.java
@@ -4,27 +4,36 @@ import com.teamtreehouse.blog.dao.BlogDAO;
 import com.teamtreehouse.blog.dao.BlogDAOImpl;
 import com.teamtreehouse.blog.model.BlogEntry;
 import spark.ModelAndView;
+import spark.Request;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.post;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class Main {
+    private static final String FLASH_MESSAGE_KEY = "flash_message";
+
     public static void main(String[] args) {
         staticFileLocation("/public");
         BlogDAO dao = new BlogDAOImpl();
 
-        get ("/", (req, res) -> {
+        before("/edit", (req, res) -> {
+            if (req.cookie("username") == null) {
+                setFlashMessage(req, "Please sign in first!");
+                res.redirect("/");
+                halt();
+            }
+        });
+
+        get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             model.put("entries", dao.findAllEntries());
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
-        post ("/new", (req, res) -> {
+        post("/edit", (req, res) -> {
             String title = req.queryParams("title");
             String creator = req.queryParams("creator");
             String blogPost = req.queryParams("blogPost");
@@ -34,15 +43,10 @@ public class Main {
             return null;
         }, new HandlebarsTemplateEngine());
 
-        /*get("/new", (req, res) -> {
-            Map<String, String> model = new HashMap<>();
-            model.put("username", req.cookie("username"));
-            return new ModelAndView(model, "new.hbs");
-        }, new HandlebarsTemplateEngine());
-
         get("/edit", (req, res) -> {
             Map<String, String> model = new HashMap<>();
             model.put("username", req.cookie("username"));
+            model.put("flashMessage", captureFlashMessage(req));
             return new ModelAndView(model, "edit.hbs");
         }, new HandlebarsTemplateEngine());
 
@@ -51,8 +55,30 @@ public class Main {
             String username = req.queryParams("username");
             res.cookie("username", username);
             model.put("username", username);
-            return new ModelAndView(model, "password.hbs");
-        }, new HandlebarsTemplateEngine());*/
+            return new ModelAndView(model, "edit.hbs");
+        }, new HandlebarsTemplateEngine());
 
+    }
+
+    private static void setFlashMessage(Request req, String message) {
+        req.session().attribute(FLASH_MESSAGE_KEY, message);
+    }
+
+    private static String getFlashMessage(Request req) {
+        if (req.session(false) == null) {
+            return null;
+        }
+        if (!req.session().attributes().contains(FLASH_MESSAGE_KEY)) {
+            return null;
+        }
+        return (String) req.session().attribute(FLASH_MESSAGE_KEY);
+    }
+
+    private static String captureFlashMessage(Request req) {
+        String message = getFlashMessage(req);
+        if (message != null) {
+            req.session().removeAttribute(FLASH_MESSAGE_KEY);
+        }
+        return message;
     }
 }
